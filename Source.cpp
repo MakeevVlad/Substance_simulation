58afee1229cf896b8012c7a3eecec30f1cba3f9e
avmmal <32042102+MakeevVlad@users.noreply.github.com>
2019-09-24 12:37:41 +0300

    Add files via upload

---
+++ Source.cpp
@@ -0,0 +1,25 @@
+#include <vector>
+#include <iostream>
+
+
+typedef std::vector<std::vector<std::vector<double>>> tdim_vec;
+
+
+
+int main()
+{
+	//Substrate, field[0] -- availability of crystal germ, fiel[1] -- potencial
+
+	size_t x, y, z;
+
+	/*
+	while(1)
+	{
+	coord(w, l);
+
+	}
+	*/
+
+}
+
+
